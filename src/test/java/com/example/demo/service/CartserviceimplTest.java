2b04d27f506777bab8ca69209421146292fa41a5
Deeksha Sharma <deekshash@hcl.com>
2019-11-05 16:22:31 +0530

    configure swagger

--- src/test/java/com/example/demo/service/CartserviceimplTest.java
+++ src/test/java/com/example/demo/service/CartserviceimplTest.java
@@ -19,7 +19,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.internal.configuration.MockAnnotationProcessor;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
